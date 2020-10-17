f141efbd67167c67db95d2e3b9cc0acc38b2876d
SeregyLipatnikov <sv.lipatnikov@gmail.com>
2020-10-17 18:08:21 +0400

    v.0.1 built-in-led

--- KitchenLight.ino
+++ KitchenLight.ino
@@ -4,18 +4,28 @@
  * 
  * v.0.1 - 17.10.2020
  * Реализация ШИМ на встроенном светодиоде
+ * analogWrite 0-1023
+ * Логика инверсная: 0 = max, 1023 = min
  */
-#define LED_PIN 1
+#define LED_PIN 2
 
-void setup() {
-  Serial.begin( 9600 );
-  pinMode(LED_PIN, OUTPUT);
+//const int linearPwmPoints[] = {0,1,2,4,8,16,32,64,128,256,512,1023};
+const int linearPwmPoints[] = {0,1,3,5,8,13,21,34,55,89,144,233,377,610,987}; //Fibonachi
+
+
+void setup() 
+{
+  pinMode(LED_PIN, OUTPUT);   
 }
 
 void loop() 
 {
-  digitalWrite(LED_PIN, HIGH);
-  delay(2000);
-  digitalWrite(LED_PIN, LOW);
-  delay(2000);
+  for (int i = 0; i <= (sizeof(linearPwmPoints)/sizeof(int)) - 1; i++) {
+    analogWrite(LED_PIN, 1023-linearPwmPoints[i]);
+    delay(100);
+  }
+  for (int i = (sizeof(linearPwmPoints)/sizeof(int)) - 1; i >= 0; i--) {
+    analogWrite(LED_PIN, 1023-linearPwmPoints[i]);
+    delay(100);
+  }
 }
